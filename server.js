9e912e484db9b25d337cc96949cd088e29d7fdce
MuriloEduardo <muriloeduardoooooo@gmail.com>
2017-04-27 16:34:22 -0300

    Corrigindo Tag Manager 2

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 "use strict"
 
-let express = require('express');
-let app = express();
+let express    = require('express');
+let app 	   = express();
 let bodyParser = require('body-parser');
-let mongoose = require('mongoose');
-let configDB = require('./server/config/database');
-let multer = require('multer');
-let morgan = require('morgan');
-let path = require("path");
-
-let options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+let mongoose   = require('mongoose');
+let configDB   = require('./server/config/database');
+let multer     = require('multer');
+let morgan     = require('morgan');
+let path       = require("path");
+let favicon    = require('serve-favicon');
+let port       = process.env.PORT || 3000;
+let api        = require('./server/routes/api');
+let options    = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
               replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
 
 mongoose.connect(configDB.url, options, (err, res) => {
-  mongoose.Promise = global.Promise;
-  if(err) throw err;
+	mongoose.Promise = global.Promise;
+	if(err) throw err;
 
-  console.info('MongoDB Conectado url: ' + configDB.url);
+	console.info('MongoDB Conectado url: ' + configDB.url);
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,14 +29,13 @@ app.use(morgan('dev'));
 app.use('/uploads', express.static('server/uploads'));
 app.use('/', express.static('dist'));
 
-let port = process.env.PORT || 3000;
-
-let api = require('./server/routes/api');
 app.use('/api', api);
 
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
+app.use(favicon(path.join(__dirname, 'server', 'images', 'favicon.ico')));
+
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('Magic happens on port: ' + port);
